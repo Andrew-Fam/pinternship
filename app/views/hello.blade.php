c4519fc21a8c8ee057e62b6002abc531eadbcb42
vagrant <vagrant@ubuntu-server-12042-x64-vbox4210.(none)>
2013-11-01 08:46:37 +0000

    basic setup for laravel x bootstrap project

---
+++ app/views/hello.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Basic laravel x bootstrap project setup</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.css')}}"/>
+    <link rel="stylesheet" href="{{asset('theme-demo/css/theme-demo.css')}}"/>
+</head>
+<body>
+	<div>
+		Basic laravel x bootstrap project setup
+	</div>
+   	 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/holder.js')}}"></script>
+</body>
+</html>
